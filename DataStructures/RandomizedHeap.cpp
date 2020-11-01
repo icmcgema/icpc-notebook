3670c52cec5062256763085f1486f860afc50228
Frederico Ribeiro <fredericobsr@gmail.com>
2020-11-01 01:54:52 -0300

    Added randomized heap

---
+++ DataStructures/RandomizedHeap.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rd(10);
+uniform_int_distribution<int> toss(0, 1);
+
+// randomized heap https://cp-algorithms.com/data_structures/randomized_heap.html
+// capable of O(log n) insertion, pop_min, melding (combining two heaps)
+// and O(1) get_min
+template<typename T>
+struct RandomizedHeap {
+    using ptr = unique_ptr<RandomizedHeap<T>>;
+
+    T val;
+    ptr l, r;
+
+    RandomizedHeap() = default;
+    RandomizedHeap(T x) : val(x), l(nullptr), r(nullptr) {};
+
+    // combines two heaps a and b
+    // consumes heap a and b, and procudes a new heap
+    friend ptr meld(ptr a, ptr b) {
+        if (!a) return b;
+        if (!b) return a;
+
+        // a is the root 
+        if (a->val > b->val) swap(a, b);
+
+        // 50/50 chance to combine b with each child of a 
+        if (toss(rd)) a->l = meld(move(a->l), move(b));
+        else a->r = meld(move(a->r), move(b));
+
+        return a;
+    }
+
+    friend void insert(ptr& a, T x) {
+        a = meld(move(a), make_unique<RandomizedHeap>(x));
+    }
+
+    friend void pop(ptr& a) {
+        a = meld(move(a->l), move(a->r));
+    }
+
+    T min() {
+        return val;
+    }
+};
+
+
+int main() {
+    RandomizedHeap<int>::ptr a, b;
+
+    for (int i = 0; i < 10; i += 2) {
+        insert(a, i);
+        insert(b, i+1);
+    }
+
+    cout << a->min() << '\n';
+    cout << b->min() << '\n';
+
+    pop(a), pop(a);
+    pop(b);
+
+    auto c = meld(move(a), move(b));
+
+    while (c) {
+        cout << c->min() << ' ';
+        pop(c);
+    }
+    cout << '\n';
+}
