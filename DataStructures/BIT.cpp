dd71fd726294012b56c6a69c81482988b0cfb396
Frederico Ribeiro <fredericobsr@gmail.com>
2020-11-01 00:38:08 -0300

    Fixed gitignore and added BIT

---
+++ DataStructures/BIT.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// can be used with all operations that have an inverse
+// can also be used in special situation if you dont need an inverse operation 
+// eg. the operation is max and the numbers added are increasing
+template<typename T>
+struct BIT {
+// if you need it to cover a dynamic range such as from 0 to 1e9:
+// change vector to map and choose an upper limit insted of b.size()s
+    vector<T> b;
+
+    BIT() = default;
+    BIT(int n) : b(n+1) {};
+
+// if you want to access position 0, add pos++ in the first line
+    void add(int pos, T x) {
+        for (int i = pos; i < (int)b.size(); i += i&-i) {
+            b[i] += x;
+        }
+    }
+
+// if you want to access position 0, add pos++ in the first line
+    T get(int pos) {
+        T r = 0;
+        for (int i = pos; i > 0; i -= i&-i) {
+            r += b[i];
+        }
+        return r;
+    }
+
+    T get(int l, int r) {
+        return get(r) - get(l-1);
+    }
+};
+
+// https://www.spoj.com/problems/INVCNT/
+int main() {
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for (auto& i : v) cin >> i;
+
+        auto com = v;
+        sort(com.begin(), com.end());
+
+        for (auto& i : v) {
+            i = lower_bound(com.begin(), com.end(), i) - com.begin() + 1;
+        }
+
+        BIT<int> bit(n);
+        long long ans = 0;
+        for (auto i : v) {
+            ans += bit.get(i+1, n);
+            bit.add(i, 1);
+        }
+
+        cout << ans << '\n';
+    }
+}
