8aac44fac12642b09217d354579b61139fcc1ed0
andrefakhoury <andreluis1704@gmail.com>
2021-01-29 17:33:30 -0300

    update java

---
+++ Misc/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+    }
+
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        // .................
+
+        out.close();
+    }
+}
