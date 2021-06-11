1afcb73e8b29bd6f71c4c609f3ec2e51bccfebcd
Lucas Fernandes Turci <lucas.turci@gmail.com>
2021-06-10 22:44:48 -0300

    Update MinQueue.cpp
    Add implementation using two stacks, which we should have because it's able to do any commutative-associative function over the elements of the queue (e.g gcd)

--- DataStructures/MinQueue.cpp
+++ DataStructures/MinQueue.cpp
@@ -34,3 +34,45 @@ struct MinQueue {
         return r-l;
     }
 };
+
+// ------------------- Another implementation, using two stacks (bigger code, but more powerful) --------------------- //
+
+template<class T, T F(T, T) = std::min<T>>
+struct minstack {
+	stack<pair<T, T> > s;
+ 
+	void push(T x) {
+		if (!s.size()) s.push({x, x});
+		else s.push({x, F(s.top().second, x)});
+	}
+	T top() { return s.top().first;	}
+	T pop() {
+		T ans = s.top().first;
+		s.pop();
+		return ans;
+	}
+	int size() { return s.size(); }
+	T min() { return s.top().second; }
+};
+ 
+template<class T, T F(T, T) = std::min<T>>
+struct minqueue {
+	minstack<T, F> s1, s2;
+ 
+	void push(T x) { s1.push(x); }
+	void move() {
+		if (s2.size()) return;
+		while (s1.size()) {
+			T x = s1.pop();
+			s2.push(x);
+		}
+	}
+	T front() { return move(), s2.top(); }
+	T pop() { return move(), s2.pop(); }
+	int size() { return s1.size()+s2.size(); }
+	T min() {
+		if (!s1.size()) return s2.min();
+		else if (!s2.size()) return s1.min();
+		return F(s1.min(), s2.min());
+	}
+};
