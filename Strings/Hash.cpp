afde414267b1fb3dfad37397ed0486489103a85b
andrefakhoury <andreluis1704@gmail.com>
2021-06-16 11:04:18 -0300

    Update Hash (add range)

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -12,36 +12,36 @@ struct Hash {
 	Hash(int x) { val[0] = x % MOD[0]; val[1] = x % MOD[1]; }
 	Hash(int x, int y) { val[0] = x % MOD[0]; val[1] = y % MOD[1]; }
 
-	static int add(int x, int y, int k) { x += y; if (x >= MOD[k]) x -= MOD[k]; return x; }
-	static int sub(int x, int y, int k) { x -= y; if (x < 0) x += MOD[k]; return x; }
-	static int mul(int x, int y, int k) { return 1ll * x * y % MOD[k]; }
-	static int fpow(int x, int y, int k) {
+	inline static int add(int x, int y, int k) { x += y; if (x >= MOD[k]) x -= MOD[k]; return x; }
+	inline static int sub(int x, int y, int k) { x -= y; if (x < 0) x += MOD[k]; return x; }
+	inline static int mul(int x, int y, int k) { return 1ll * x * y % MOD[k]; }
+	inline static int fpow(int x, int y, int k) {
 		int r = 1;
 		for (; y > 0; y /= 2, x = mul(x, x, k))
 			if (y % 2 == 1) r = mul(r, x, k);
 		return r;
 	}
-	static int divi(int x, int y, int k) { return mul(x, fpow(y, MOD[k] - 2, k), k); }
-	static Hash pow(Hash x, int y) {
+	inline static int divi(int x, int y, int k) { return mul(x, fpow(y, MOD[k] - 2, k), k); }
+	inline static Hash pow(Hash x, int y) {
 		Hash r = 1;
 		for (; y >= 0; y /= 2, x *= x)
 			if (y%2 == 1) r *= x;
 		return r;
 	}
 
-	Hash operator+(Hash const& h) const { return Hash(add(val[0], h.val[0], 0), add(val[1], h.val[1], 1)); }
-	Hash operator-(Hash const& h) const { return Hash(sub(val[0], h.val[0], 0), sub(val[1], h.val[1], 1)); }
-	Hash operator*(Hash const& h) const { return Hash(mul(val[0], h.val[0], 0), mul(val[1], h.val[1], 1)); }
-	Hash operator/(Hash const& h) const { return Hash(divi(val[0], h.val[0], 0), divi(val[1], h.val[1], 1)); }
-	Hash& operator+=(Hash const& h) { return *this = *this + h; }
-	Hash& operator-=(Hash const& h) { return *this = *this - h; }
-	Hash& operator*=(Hash const& h) { return *this = *this * h; }
-	Hash& operator/=(Hash const& h) { return *this = *this / h; }
+	inline Hash operator+(Hash const& h) const { return Hash(add(val[0], h.val[0], 0), add(val[1], h.val[1], 1)); }
+	inline Hash operator-(Hash const& h) const { return Hash(sub(val[0], h.val[0], 0), sub(val[1], h.val[1], 1)); }
+	inline Hash operator*(Hash const& h) const { return Hash(mul(val[0], h.val[0], 0), mul(val[1], h.val[1], 1)); }
+	inline Hash operator/(Hash const& h) const { return Hash(divi(val[0], h.val[0], 0), divi(val[1], h.val[1], 1)); }
+	inline Hash& operator+=(Hash const& h) { return *this = *this + h; }
+	inline Hash& operator-=(Hash const& h) { return *this = *this - h; }
+	inline Hash& operator*=(Hash const& h) { return *this = *this * h; }
+	inline Hash& operator/=(Hash const& h) { return *this = *this / h; }
 
-	bool operator==(Hash const& h) const { return val[0] == h.val[0] && val[1] == h.val[1]; }
-	bool operator!=(Hash const& h) const { return val[0] != h.val[0] || val[1] != h.val[1]; }
+	inline bool operator==(Hash const& h) const { return val[0] == h.val[0] && val[1] == h.val[1]; }
+	inline bool operator!=(Hash const& h) const { return val[0] != h.val[0] || val[1] != h.val[1]; }
 
-	static Hash calculateHash(string const& s, Hash const primes = Hash(31, 37)) {
+	inline static Hash calculateHash(string const& s, Hash const primes = Hash(31, 37)) {
 		Hash cur = 0;
 		Hash p = 1;
 		for (char c : s) {
@@ -50,7 +50,7 @@ struct Hash {
 		}
 		return cur;
 	}
-	static vector<Hash> calculateHashVector(string const& s, Hash const primes = Hash(31, 37)) {
+	inline static vector<Hash> calculateHashVector(string const& s, Hash const primes = Hash(31, 37)) {
 		int n = s.size();
 		Hash p = 1;
 		vector<Hash> cur(n);
@@ -61,12 +61,15 @@ struct Hash {
 		}
 		return cur;
 	}
-	static vector<Hash> calculatePowerVector(Hash p, const int n) {
+	inline static vector<Hash> calculatePowerVector(Hash p, const int n) {
 		vector<Hash> ans(n);
 		ans[0] = 1;
 		for (int i = 1; i < n; i++)
 			ans[i] = ans[i-1] * p;
 		return ans;
+	}
+	inline static Hash getRange(vector<Hash> const& v, vector<Hash> const& invPow, int l, int r) {
+		return (v[r] - (l ? v[l-1] : 0)) * invPow[l];
 	}
 };
 
@@ -76,6 +79,15 @@ ostream& operator<<(ostream& out, Hash const& h) {
 
 #define hash UISHDUIAHSDU
 
+/** NOTES:
+ *    at the beginning, precalc:
+ * 		vector<Hash> hash = Hash::calculateHashVector(s, primes);
+ * 		vector<Hash> power = Hash::calculatePowerVector(primes, n);
+ * 		vector<Hash> invPow = Hash::calculatePowerVector(Hash(1)/primes, n);
+ * */
+
+// =================================================================================== //
+
 inline void minimalRotation(string s) {
 	int n = s.size();
 	s += s;
@@ -83,6 +95,7 @@ inline void minimalRotation(string s) {
 	Hash primes(31, 29);
 	vector<Hash> hash = Hash::calculateHashVector(s, primes);
 	vector<Hash> power = Hash::calculatePowerVector(primes, n);
+	vector<Hash> invPow = Hash::calculatePowerVector(Hash(1)/primes, n);
 
 	// lexicographically compares two substrings: s[A..A+n] and s[B..B+n]
 	auto cmp = [&](int const A, int const B) {
@@ -92,11 +105,8 @@ inline void minimalRotation(string s) {
 
 		while(lo < hi) {
 			mi = (lo + hi) / 2;
-			h1 = hash[A + mi - 1] - (A ? hash[A - 1] : 0);
-			h2 = hash[B + mi - 1] - (B ? hash[B - 1] : 0);
-			if (A < B) h1 *= power[B - A];
-			else h2 *= power[A - B];
-
+			Hash h1 = Hash::getRange(hash, invPow, A, A + mi - 1);
+			Hash h2 = Hash::getRange(hash, invPow, B, B + mi - 1);
 			if (h1 != h2) hi = mi;
 			else lo = mi + 1;
 		}
