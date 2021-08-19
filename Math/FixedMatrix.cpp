1dbfc9825ce704b1d86298590ddbaf9096705885
Frederico Ribeiro <fredericobsr@gmail.com>
2021-08-19 00:13:39 -0300

    Fixed indexes, exponentiation without explicit modulus, removed non-default constructor and added constexpr.

--- Math/FixedMatrix.cpp
+++ Math/FixedMatrix.cpp
@@ -6,41 +6,45 @@ using ll = long long;
 
 template<typename T, int n, int m, int mod = -1>
 struct Matrix {
+	static constexpr bool has_mod = (mod == -1);
+
 	array<T, n*m> mat = {};
 
 	// matrix indexing is done via operator()
 	// ie for a matrix m, m(1, 0), accesses the first element of the second row
-	inline T const& operator()(int i, int j) const {
+	constexpr inline T const& operator()(int i, int j) const {
 		return mat[i*m + j];
 	}
 
-	inline T& operator()(int i, int j) {
+	constexpr inline T& operator()(int i, int j) {
 		return mat[i*m + j];
 	}
 
-	// constructors
-	Matrix(vector<vector<T>> const& v) {
+	// static constructors
+	static Matrix from_vecs(vector<vector<T>> const& v) {
+		Matrix r;
         for (int i = 0; i < n; i++) {
-            copy(v[i].begin(), v[i].end(), mat.begin()+m*i);
+            copy(v[i].begin(), v[i].end(), r.mat.begin() + i*m);
         }
+		return r;
     }
 
-	static Matrix identity() {
+	constexpr static Matrix identity() {
 		Matrix r{};
 		for(int i = 0; i < n; ++i) r(i, i) = 1;
 		return r;
 	}
 
-	static Matrix zeros() {
+	constexpr static Matrix zeros() {
 		return Matrix{};
 	}
 
 	// operators
-	Matrix& operator+=(Matrix const& rhs) {
+	constexpr Matrix& operator+=(Matrix const& rhs) {
 		for(int i = 0; i < n; ++i) {
 			for(int j = 0; j < m; ++j) {
 				(*this)(i, j) += rhs(i, j);
-				if constexpr (mod != -1) {
+				if constexpr (has_mod) {
 					if ((*this)(i, j) >= mod) (*this)(i, j) -= mod;
 				}
 			}
@@ -48,16 +52,16 @@ struct Matrix {
 		return *this;
 	}
 
-	Matrix operator+(Matrix const& rhs) const {
+	constexpr Matrix operator+(Matrix const& rhs) const {
 		auto ans = *this;
 		return ans += rhs;
 	}
 
-	Matrix& operator-=(Matrix const& rhs) {
-		for(int i = 0; i < n; ++i) {
-			for(int j = 0; j < m; ++j) {
+	constexpr Matrix& operator-=(Matrix const& rhs) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
 				(*this)(i, j) -= - rhs(i, j);
-				if constexpr (mod != -1) {
+				if constexpr (has_mod) {
 					if ((*this)(i, j) < 0) (*this)(i, j) += mod;
 				}
 			}
@@ -65,19 +69,19 @@ struct Matrix {
 		return *this;
 	}
 
-	Matrix operator-(Matrix const& rhs) const {
+	constexpr Matrix operator-(Matrix const& rhs) const {
 		auto ans = *this;
 		return ans -= rhs;
 	}
 
 	template<int p>
-	Matrix<T, n, p, mod> operator*(Matrix<T, m, p, mod> const& rhs) const {
+	constexpr Matrix<T, n, p, mod> operator*(Matrix<T, m, p, mod> const& rhs) const {
 		Matrix<T, n, p, mod> res{};
 		for (int i = 0; i < n; i++) {
 			// for large matrices swap the next two lines for better cache locality
 			for (int j = 0; j < p; j++) {
 				for (int k = 0; k < m; k++) {
-					if constexpr (mod != -1) res(i, j) = ((ll)res(i, j) + (ll)(*this)(i, k) * rhs(k, j)) % mod;
+					if constexpr (has_mod) res(i, j) = ((ll)res(i, j) + (ll)(*this)(i, k) * rhs(k, j)) % mod;
 					else res(i, j) += (*this)(i, k) * rhs(k, j);
 				}
 			}
@@ -85,24 +89,38 @@ struct Matrix {
 		return res;
 	}
 
-	Matrix& operator*=(T x) {
+	constexpr Matrix<T, n, n, mod> operator^(int k) const {
+		// garantees matrix is square
+		static_assert(n == m, "n != m");
+
+		auto a = *this;
+		auto ans = Matrix<T, n, n, mod>::identity();
+		while(k) {
+			if(k&1) ans = ans * a;
+			a = a * a;
+			k >>= 1;
+		}
+		return ans;
+	}
+
+	constexpr Matrix& operator*=(T x) {
 		for (auto& i : mat) {
-			if constexpr (mod != -1) i = 1ll * i * x % mod;
+			if constexpr (has_mod) i = 1ll * i * x % mod;
 			else i *= x;
 		}
 		return *this;
 	}
 
-	Matrix operator*(T x) const {
+	constexpr Matrix operator*(T x) const {
 		auto ans = *this;
 		return ans *= x;
 	}
 
-	bool operator==(Matrix<T, n, m, mod> const& rhs) const {
+	constexpr bool operator==(Matrix<T, n, m, mod> const& rhs) const {
 		return equal(mat.begin(), mat.end(), rhs.mat.begin());
 	}
 
-	Matrix<T, m, n, mod> transposed() {
+	constexpr Matrix<T, m, n, mod> transposed() const {
 		auto ans = Matrix<T, m, n, mod>{};
 		for(int i = 0; i < n; ++i) {
 			for(int j = 0; j < m; ++j) {
@@ -121,17 +139,4 @@ struct Matrix {
 		}
 		return os;
 	}
-
 };
-
-// garantees matrix is square
-template<typename T, int n, int mod = -1>
-Matrix<T, n, n, mod> operator^(Matrix<T, n, n, mod> a, ll k) {
-	auto ans = Matrix<T, n, n, mod>::identity();
-	while(k) {
-		if(k&1) ans = ans * a;
-		a = a * a;
-		k >>= 1;
-	}
-	return ans;
-}
